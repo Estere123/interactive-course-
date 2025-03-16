09ee133405e815a5c5cda93ad986dccf2722b2e2
Estere123 <estere.mitule@gmail.com>
2025-03-16 21:55:02 +0200

    HW5
    Started coding

---
+++ HW5/app1/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Board from './Components/Board';
+import Square from './Components/Square';
+import './styles.css';
+
+
+function App() {
+  
+
+  return (
+    <>
+   
+   <Board/>
+   
+   
+    </>
+  )
+}
+
+export default App
