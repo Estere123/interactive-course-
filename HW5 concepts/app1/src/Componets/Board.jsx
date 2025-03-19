d63f712b5498ea7be53e558dcf1b24e3cebfea95
Estere123 <estere.mitule@gmail.com>
2025-03-19 10:25:47 +0200

    HW5 concepts
    replicated concept of calculationg winner

---
+++ HW5 concepts/app1/src/Componets/Board.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Board() {
+    function handleClick(i) {
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+          nextSquares[i] = 'X';
+        } else {
+          nextSquares[i] = 'O';
+        }
+        onPlay(nextSquares);
+      }
+  
+      const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+  
+  
+   
+}
